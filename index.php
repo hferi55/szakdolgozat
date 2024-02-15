cfded5deb9b1d91bd049c4fa37e6cc8450b8f346
hferi55 <hferi555@gmail.com>
2024-02-15 09:44:18 +0100

    A weboldal fejléc
    A weboldal fejlécének megcsinálása

--- index.php
+++ index.php
@@ -5,29 +5,27 @@
 <!DOCTYPE html>
 <html lang="hu">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE-edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Étrendkészítő Weboldal</title>
-  <!-- CSS -->
-  <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Étrend Készítő</title>
+    <link rel="stylesheet" href="style.css">
+
 </head>
 <body>
 
-<div class="container">
-    <div class="regisztracio kartya">
-      <header>Főoldal</header>
-      <form action="regisztracio.php" method="post">
-          <label>Még nincs fiókja?</label>
-          <input type="submit" class="button" value="Regisztráció" name="submit">
-      </form>
+<header class="fooldalheader">
+    <h1 class="cim">ÉKW</h1>
+    <nav class="navbar">
+        <a href="rolunk.php">Rólunk</a> |
+        <a href="bejelentkezes.php">Bejelntkezés</a> |
+        <a href="regisztracio.php">Regisztráció</a> 
+</nav>
+</header>
 
-      <form action="bejelentkezes.php" method="post">
-          <label>Már van fiókja?</label>
-          <input type="submit" class="button" value="Bejelentkezés" name="submit">
-      </form>
-    </div>
-</div>
+<main>
+    
+
+</main>
 
 </body>
 </html>
