32bdd9b7126fadcce7dd3bd161840df6b4be3e17
hferi55 <hferi555@gmail.com>
2024-02-15 09:53:29 +0100

    Új oldalak és hivatkozások létrehozása
    A fejlécben lévő szavakkal lehet rájuk hivatkozni mint pl.: profil.php ahol majd a profil beállításait lehet majd beállítani

---
+++ loggedin.php
@@ -0,0 +1,38 @@
+<?php
+  require("sql.php");
+?>
+
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE-edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Étrendkészítő Weboldal</title>
+  <!-- CSS -->
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <header class="fooldalheader">
+      <h1 class="cim">ÉKW</h1>
+          <nav class="navbar">
+              <a href="rolunk.php">Rólunk</a> |
+              <a href="profil.php">Profil</a> |
+              <a href="etrendkeszites.php">Étrendkészítése</a> 
+          </nav>
+    </header>
+
+
+
+<div class="container">
+    <div class="kartya">
+      <form action="" method="post">
+          <label>Még nincs fiókja?</label>
+         
+      </form>
+    </div>
+</div>
+
+</body>
+</html>
