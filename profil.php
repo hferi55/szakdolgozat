e18d5eb989ec8bb6382655cae396caa2aba5e4b4
hferi55 <hferi555@gmail.com>
2024-02-21 11:55:41 +0100

    profilkép feltöltése
    profilkép feltöltése meg csinálása, kezdetleges megoldás, mert a profilkép csak addig van ott amíg a felhasználó be van jelentkezve

--- profil.php
+++ profil.php
@@ -1,41 +1,45 @@
-<?php
-  require("sql.php");
-?>
-
 <!DOCTYPE html>
 <html lang="hu">
 <head>
   <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE-edge">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Étrendkészítő Weboldal</title>
-  <!-- CSS -->
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
-    <header class="fooldalheader">
-        <h1 class="cim">ÉKW</h1>
-            <nav class="navbar">
-                <a href="rolunk2.php">Rólunk</a> |
-                <a href="profil.php">Profil</a> |
-                <a href="etrendkeszitese.php">Étrendkészítése</a> |
-                <a href="logout.php">Kijelentkezés</a>
-            </nav>
-    </header>
-
+<header class="fooldalheader">
+    <h1 class="cim">ÉKW</h1>
+    <nav class="navbar">
+        <a href="rolunk2.php">Rólunk</a> |
+        <a href="profil.php">Profil</a> |
+        <a href="etrendkeszitese.php">Étrendkészítése</a> |
+        <a href="logout.php">Kijelentkezés</a>
+    </nav>
+</header>
 
 <div class="profil_lap">
     <div class="kartya">
     <header>Profil</header>
-    <form action="profilkep_feltoltes.php" method="post" enctype="multipart/form-data">
+      <form action="profilkep_feltoltes.php" method="post" enctype="multipart/form-data">
 
-    <h3>Profilkép</h3>
-    <img src="kepek/uresprofilkep.png" alt="Profilkép" class="profilkep">
-    <br>
-    <label for="profilkep_feltoltes">Profilkép kicserélése:</label>
-    <input type="file" id="profilkep_feltoltes" name="profilkep" accept="image/*">
-    <br>
+      <h3>Profilkép</h3>
+      <?php
+      session_start();
+      
+      // Profilkép elérési útjának lekérése a session-ből
+      $profilkep = isset($_SESSION['profilkep']) ? $_SESSION['profilkep'] : "profilkepek/uresprofilkep.png";
+      ?>
+      
+      <!-- A profilkep változó használata a profilkép megjelenítéséhez -->
+      <img src="<?php echo htmlspecialchars($profilkep); ?>" alt="Profilkép" class="profilkep">
+
+      <br>
+      <!-- Fájlfeltöltő input elem hozzáadása -->
+      <label for="profilkep_feltoltes">Profilkép kicserélése:</label>
+      <input type="file" id="profilkep_feltoltes" name="profilkep" accept="image/*">
+      <br>
 
       <h3>Név</h3>
       <input type="text" placeholder="Név" name="nev">
