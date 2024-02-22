b2161ffdd493d6d89527a1ec90e033422661e692
hferi55 <hferi555@gmail.com>
2024-02-22 13:15:22 +0100

    webszerverre való átírás
    a $username, $password, $dbname-t átírtam hogy az oldal a szerverről legyen elérhető

--- sql.php
+++ sql.php
@@ -1,9 +1,9 @@
 <?php
 
 $servername = "localhost"; 
-$username = "admin"; 
-$password = "qCvE_CfYzgZoMX4t"; 
-$dbname = "szakdolgozat"; 
+$username = "c31harsanyiFR"; 
+$password = "PefZwG9@3"; 
+$dbname = "c31harsanyiFR_db"; 
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
