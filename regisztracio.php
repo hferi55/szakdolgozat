6466623cea9e74b080db8b900262c712d3ce8092
hferi55 <hferi555@gmail.com>
2024-02-29 10:25:46 +0100

    regisztrációs oldal módosítása

--- regisztracio.php
+++ regisztracio.php
@@ -15,9 +15,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Ellenőrzés, hogy az összes mező ki van-e töltve
     if (!empty($nev) && !empty($email) && !empty($jelszo) && !empty($jelszoMegerosit)) {
         // Ellenőrzés, hogy a név megfelelő hosszúságú
-        if (strlen($nev) >= 3) {
+        if (strlen($nev) >= 3 && !preg_match('/[^A-Za-z0-9]/', $nev && strlen($nev)) != 0 && strlen($nev) <= 8 && !empty($nev)) {
             // Ellenőrzés, hogy a jelszó megfelelő hosszúságú, tartalmaz-e nagybetűt, és nem tartalmaz-e speciális karaktert
-            if (strlen($jelszo) >= 4 && preg_match('/[A-Z]/', $jelszo) && !preg_match('/[^A-Za-z0-9]/', $jelszo)) {
+            if (strlen($jelszo) >= 4 && preg_match('/[A-Z]/', $jelszo) && !preg_match('/[^A-Za-z0-9]/', $jelszo) && !empty($jelszo)) {
                 // Biztonságosabbá tétele az SQL lekérdezéseknek
                 $nev = mysqli_real_escape_string($conn, $nev);
                 $email = mysqli_real_escape_string($conn, $email);
@@ -89,8 +89,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
             <form action="" method="post">
                 <input type="text" placeholder="Adja meg a nevét" name="nev" required>
+                <label>A név minimum 3, maximum 8 karakter lehet, nem tartalmazhat speciális karaktert.</label>
+                <br>
+                <br>
                 <input type="email" placeholder="Adja meg az email címét" name="email" required>
                 <input type="password" placeholder="Adja meg a jelszavát" name="jelszo" required>
+                <label>A jelszó minimum 4 karakter lehet, nem tartalmazhat speciális karaktert, minimum 1 nagy karaktert kell tartalmaznia.</label>
+                <br>
+                <br>
                 <input type="password" placeholder="Erősítse meg a jelszavát" name="jelszo_megerosit" required>
                 <input type="submit" class="button" value="Regisztráció" name="submit" required>
             </form>
