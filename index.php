880dbf41512c999db071be9f36b6fd86e925a6d8
hferi55 <101869059+hferi55@users.noreply.github.com>
2024-03-12 20:22:19 +0100

    fejléc és megjelenítő adatok módosítása

--- index.php
+++ index.php
@@ -16,16 +16,52 @@
 <header class="fooldalheader">
     <h1 class="cim">ÉKW</h1>
     <nav class="navbar">
-        <a href="view/rolunk1.php">Rólunk</a> |
-        <a href="view/bejelentkezes.php">Bejelentkezés</a> |
-        <a href="view/regisztracio.php">Regisztráció</a> 
-</nav>
+        <?php
+        // Ha a felhasználó nincs bejelentkezve
+        if (!isset($_SESSION['felhasznalo_id'])) {
+            echo '
+            <a href="view/rolunk.php">Rólunk</a> |
+            <a href="view/bejelentkezes.php">Bejelentkezés</a> |
+            <a href="view/regisztracio.php">Regisztráció</a>
+            ';
+        } else { // Ha a felhasználó be van jelentkezve
+            $felhasznalo_id = $_SESSION['felhasznalo_id'];
+            // Ellenőrizzük, hogy van-e már étrendje
+            $query = "SELECT COUNT(*) FROM felhasznalo WHERE felhasznalo_id = $felhasznalo_id AND magassag IS NOT NULL AND testsuly IS NOT NULL AND cel IS NOT NULL AND nem IS NOT NULL AND eletkor IS NOT NULL AND aktivitas IS NOT NULL";
+            $result = mysqli_query($conn, $query);
+            $row = mysqli_fetch_row($result);
+            $etrendVan = $row[0] > 0;
+
+            if ($etrendVan) {
+                echo '
+                <a href="../view/rolunk.php">Rólunk</a> |
+                <a href="../view/profil.php">Profil</a> |
+                <a href="../view/etrend.php">Étrendem</a> |
+                <a href="../logout.php">Kijelentkezés</a>
+                ';
+            } else {
+                echo '
+                <a href="../view/rolunk.php">Rólunk</a> |
+                <a href="../view/profil.php">Profil</a> |
+                <a href="../view/etrendkeszitese.php">Étrendkészítés</a> |
+                <a href="../logout.php">Kijelentkezés</a>
+                ';
+            }
+        }
+        ?>
+    </nav>
 </header>
 
-<main>
-    
+<div class="lap">
+    <div class="kartya">
+      <header>Főoldal</header>
+      <form action="" method="post">
+          <label>Főoldal</label>
+         
+      </form>
+    </div>
+</div>
 
-</main>
 
 </body>
 </html>
