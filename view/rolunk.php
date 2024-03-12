d790d777a5adee246faa0fd7b650decdfcd07e90
hferi55 <101869059+hferi55@users.noreply.github.com>
2024-03-12 20:17:54 +0100

    A Rólunk oldal átdolgozása
    A két Rólunk oldal egy Rólunk oldallá át dolgozása

--- view/rolunk.php
+++ view/rolunk.php
@@ -1,7 +1,6 @@
 <?php
-  require("../sql/sql.php");
-
-  $bejelentkezve = isset($_POST['email']) && !empty($_POST['email']);
+require("../sql/sql.php");
+session_start(); // Session inicializálása
 
 ?>
 
@@ -17,14 +16,46 @@
 </head>
 <body>
 
-    <header class="fooldalheader">
-        <h1 class="cim">ÉKW</h1>
-            <nav class="navbar">
-                <a href="#">Rólunk</a> |
-                <a href="../view/bejelentkezes.php">Bejelentkezés</a> |
-                <a href="../view/regisztracio.php">Regisztráció</a>
-            </nav>
-    </header>
+<header class="fooldalheader">
+    <h1 class="cim">ÉKW</h1>
+    <nav class="navbar">
+        <?php
+        // Ha a felhasználó nincs bejelentkezve
+        if (!isset($_SESSION['felhasznalo_id'])) {
+            echo '
+            <a href="../view/rolunk.php">Rólunk</a> |
+            <a href="../view/bejelentkezes.php">Bejelentkezés</a> |
+            <a href="../view/regisztracio.php">Regisztráció</a>
+            ';
+        } else { // Ha a felhasználó be van jelentkezve
+            require("../sql/sql.php");
+            $felhasznalo_id = $_SESSION['felhasznalo_id'];
+            // Ellenőrizzük, hogy van-e már étrendje
+            $query = "SELECT COUNT(*) FROM felhasznalo WHERE felhasznalo_id = $felhasznalo_id AND magassag IS NOT NULL AND testsuly IS NOT NULL AND cel IS NOT NULL AND nem IS NOT NULL AND eletkor IS NOT NULL AND aktivitas IS NOT NULL";
+            $result = mysqli_query($conn, $query);
+            $row = mysqli_fetch_row($result);
+            $etrendVan = $row[0] > 0;
+
+            if ($etrendVan) {
+                echo '
+                <a href="../view/rolunk.php">Rólunk</a> |
+                <a href="../view/profil.php">Profil</a> |
+                <a href="../view/etrend.php">Étrendem</a> |
+                <a href="../logout.php">Kijelentkezés</a>
+                ';
+            } else {
+                echo '
+                <a href="../view/rolunk.php">Rólunk</a> |
+                <a href="../view/profil.php">Profil</a> |
+                <a href="../view/etrendkeszitese.php">Étrendkészítés</a> |
+                <a href="../logout.php">Kijelentkezés</a>
+                ';
+            }
+        }
+        ?>
+    </nav>
+</header>
+
 
 <div class="lap">
     <div class="kartya">
