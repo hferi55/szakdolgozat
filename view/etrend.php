46d9f6eb9b480b9843c39a38d41acca638a793e8
hferi55 <hferi555@gmail.com>
2024-03-14 13:08:08 +0100

    étrend ki írása funkció kezdetleges megoldása

--- view/etrend.php
+++ view/etrend.php
@@ -67,8 +67,11 @@ if (isset($_SESSION['etrend_keszites_sikeres']) && $_SESSION['etrend_keszites_si
 
 
 <div class="etrend_lap">
-    <div class="kartya">
-      <header>Étrendem</header>
+    <header>Étrendem</header>
+    
+
+    
+    <div class="adatok">
       <form action="" method="post">
       <?php
         // Lekérdezés az adatok megjelenítéséhez
@@ -119,6 +122,7 @@ if (isset($_SESSION['etrend_keszites_sikeres']) && $_SESSION['etrend_keszites_si
         }
 
     ?>
+        <header>Adatok</header>
 
         <p><b>Életkor: </b><?php echo htmlspecialchars($eletkor); ?> éves</p>
         <p><b>Testsúly: </b><?php echo htmlspecialchars($testsuly); ?> kg</p>
@@ -131,7 +135,79 @@ if (isset($_SESSION['etrend_keszites_sikeres']) && $_SESSION['etrend_keszites_si
 
         <input type="submit" class="button" value="Adatok módosítása" name="adatmodositas">
 
+        <p><b>
+            A jobb oldalon található képekből kérem jelöljön 
+        </b></p>
+        <p><b>
+            meg legalább kettő olyan ételt amit kedvel
+        </b></p>
         </form>
+    </div>
+    
+    <header>Preferencia</header>
+
+    <!-- Reggeli -->
+    <div class="reggeli"> 
+        <h3>Reggeli:</h3>
+        <img src="../kepek/reggeli1.jpg" class="kepek" alt="Zabkása gyümölccsel és mandulával">
+        <img src="../kepek/reggeli2.jpg" class="kepek" alt="Görög joghurt gyümölcsökkel és mézzel">
+        <img src="../kepek/reggeli3.jpg" class="kepek" alt="Tojásrántotta zöldségekkel">
+        <img src="../kepek/reggeli4.jpg" class="kepek" alt="Avokádós teljes kiőrlésű pirítós">
+        <img src="../kepek/reggeli5.jpg" class="kepek" alt="Banános zabkeksz">
+        <img src="../kepek/reggeli6.jpg" class="kepek" alt="Chia-puding gyümölcsökkel">
+        <img src="../kepek/reggeli7.jpg" class="kepek" alt="Túrós zabkeksz">
+        <img src="../kepek/reggeli8.jpg" class="kepek" alt="Birsalma és fahéjas zabkása">
+        <img src="../kepek/reggeli9.jpg" class="kepek" alt="Avokádó és paradicsom omlett">
+        <img src="../kepek/reggeli10.jpg" class="kepek" alt="Gyors smoothie tál">
+
+    </div>
+
+    <!-- Ebéd -->
+    <div class="ebed">
+        <h3>Ebéd:</h3>
+        <img src="../kepek/ebed1.jpg" class="kepek" alt="Sült csirke salátával">
+        <img src="../kepek/ebed2.jpg" class="kepek" alt="Quinoa zöldségekkel">
+        <img src="../kepek/ebed3.jpg" class="kepek" alt="Lencseleves spenóttal">
+        <img src="../kepek/ebed4.jpg" class="kepek" alt="Tonhalas teljes kiőrlésű wrap">
+        <img src="../kepek/ebed5.jpg" class="kepek" alt="Sült lazac édesburgonyával">
+        <img src="../kepek/ebed6.jpg" class="kepek" alt="Színes borsókrémleves">
+        <img src="../kepek/ebed7.jpg" class="kepek" alt="Szezámmagos csirke saláta">
+        <img src="../kepek/ebed8.jpg" class="kepek" alt="Quinoa saláta fetával és görög olívával">
+        <img src="../kepek/ebed9.jpg" class="kepek" alt="Zöldséges tojás wrap">
+        <img src="../kepek/ebed10.jpg" class="kepek" alt="Sütőben sült lazac spárgával">
+
+    </div>
+
+    <!-- Vacsora -->
+    <div class="vacsora">
+        <h3>Vacsora:</h3>
+        <img src="../kepek/vacsora1.jpg" class="kepek" alt="Vegetáriánus csicseriborsó curry">
+        <img src="../kepek/vacsora2.jpg" class="kepek" alt="Grillezett zöldségek tofuval">
+        <img src="../kepek/vacsora3.jpg" class="kepek" alt="Sült csirkecomb sült zöldségekkel">
+        <img src="../kepek/vacsora4.jpg" class="kepek" alt="Brokkoli spagetti fokhagymás olívaolajjal">
+        <img src="../kepek/vacsora5.jpg" class="kepek" alt="Sushi tál lazaccal és zöldségekkel">
+        <img src="../kepek/vacsora6.jpg" class="kepek" alt="Vegetáriánus lencsédal">
+        <img src="../kepek/vacsora7.jpg" class="kepek" alt="Fűszeres csirke curry">
+        <img src="../kepek/vacsora8.jpg" class="kepek" alt="Brokkoli-karfiol pite">
+        <img src="../kepek/vacsora9.jpg" class="kepek" alt="Mexikói csirke quinoa-val">
+        <img src="../kepek/vacsora10.jpg" class="kepek" alt="Grillezett hal filé édesburgonya pürével">
+
+    </div>
+
+    <!-- Uzsonna -->
+    <div class="uzsonna">
+        <h3>Uzsonna:</h3>
+        <img src="../kepek/uzsonna1.jpg" class="kepek" alt="Mandula és mazsola mix">
+        <img src="../kepek/uzsonna2.jpg" class="kepek" alt="Görög joghurt gyümölcssaláttal">
+        <img src="../kepek/uzsonna3.jpg" class="kepek" alt="Almás mogyoróvaj szendvics">
+        <img src="../kepek/uzsonna4.jpg" class="kepek" alt="Zöldségek hummusszal">
+        <img src="../kepek/uzsonna5.jpg" class="kepek" alt="Banános és epres smoothie">
+        <img src="../kepek/uzsonna6.jpg" class="kepek" alt="Szezámmagos alma szeletek">
+        <img src="../kepek/uzsonna7.jpg" class="kepek" alt="Gyümölcsös joghurt pohárban">
+        <img src="../kepek/uzsonna8.jpg" class="kepek" alt="Teljes kiőrlésű kenyér paradicsomsalátával">
+        <img src="../kepek/uzsonna9.jpg" class="kepek" alt="Avokádó toast tojással">
+        <img src="../kepek/uzsonna10.jpg" class="kepek" alt="Céklás és répás smoothie">      
+
     </div>
 </div>
 
